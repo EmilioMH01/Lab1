9fbe3b4a2cf97dbc3429683575d6bb57c5aed8a3
EmiMH <47114988+EmilioMH01@users.noreply.github.com>
2022-09-08 13:51:34 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,1 +1,5 @@
-
+def get_adj_closes(tickers, start_date=None, end_date=None):
+    closes = web.DataReader(name=tickers, data_source='yahoo', start=start_date, end=end_date)
+    closes = closes['Adj Close']
+    closes.sort_index(inplace=True)
+    return closes
